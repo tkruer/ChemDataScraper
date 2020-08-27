5f69e5134ed8be5160e56e6237c659774ada5b4f
Tyler Kruer <tylerkruer1@gmail.com>
2020-08-26 20:54:28 -0700

    Add files via upload

--- ChemDataScraper/main.py
+++ ChemDataScraper/main.py
@@ -9,24 +9,27 @@ def choices():
     print(40 * "_")
     print(3 * " " + "Select the value below to retrieve")
     print(40 * "_")
-    print('{:>23}'.format("INCHI[0]"))
-    print('{:>23}'.format("INCHIKEY[1]"))
-    print('{:>23}'.format("MOLECULAR FORMULA[2]"))
-    print('{:>23}'.format("SMILES[3]"))
-    print('{:>23}'.format("MOLECULAR WEIGHT[4]"))
+    print('{:>23}'.format("InChi[0]"))
+    print('{:>23}'.format("InChiKey[1]"))
+    print('{:>23}'.format("Molecular Formula[2]"))
+    print('{:>23}'.format("Smiles[3]"))
+    print('{:>23}'.format("Molecular Weight[4]"))
+    print('{:>23}'.format("IUPAC Name[5]"))
+    print('{:>23}'.format("Charge[6]"))
+    print('{:>23}'.format("Mass for Mass Spec[7]"))
     print(40 * "_")
     global idChoice
     idChoice = int(input("Enter a number choice? "))
-    if 0 <= idChoice <= 4:
+    if 0 <= idChoice <=7:
         choiceID()
-    elif idChoice != range(0,4):
+    elif idChoice != range(0,7):
         print(2 * '\n' + 38 * '*')
         print("* Incorrect Number Choice, Try Again *")
         print(38 * '*' + 2 * '\n')
         choices()
 
 def choiceID():
-    inputList = ['INCHI', 'INCHIKEY', 'MolecularFormula','CanonicalSMILES', 'MolecularWeight']
+    inputList = ['INCHI', 'INCHIKEY', 'MolecularFormula','CanonicalSMILES', 'MolecularWeight', 'IUPACName', 'Charge', 'ExactMass']
     selChoice = inputList[idChoice]
 
     string1 = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/"
